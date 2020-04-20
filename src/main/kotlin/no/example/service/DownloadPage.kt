4fe2eddbf74e0fa5b7792617802b658aadb19326
Dmytro Chasovskyi <dmytro.chasovskyi@greenbird.com>
2020-04-20 17:59:16 +0200

    Add draft blog post for Kotlin

--- src/main/kotlin/no/example/service/DownloadPage.kt
+++ src/main/kotlin/no/example/service/DownloadPage.kt
@@ -4,6 +4,7 @@ import java.io.BufferedReader
 import java.lang.Exception
 import java.net.HttpURLConnection
 import java.net.URL
+import java.net.UnknownHostException
 
 class DownloadPage(private val page: String) {
 
@@ -12,30 +13,22 @@ class DownloadPage(private val page: String) {
      * Result<T> type limitations and current state of art in Kotlin 1.3
      * https://stackoverflow.com/questions/52631827/why-cant-kotlin-result-be-used-as-a-return-type
      */
-    fun content(): BufferedReader {
+    @Throws(UnknownHostException::class)
+    fun content(): BufferedReader{
         val url = URL(this.page)
 
         with(url.openConnection() as HttpURLConnection) {
             requestMethod = "GET"
-
-            println("\nSent 'GET' request to URL : $url; Response Code : $responseCode")
-
             return inputStream.bufferedReader()
         }
     }
 
     fun wrappedContent() : List<Result<BufferedReader>> {
-        try {
-            val url = URL(this.page)
-            with(url.openConnection() as HttpURLConnection) {
-                requestMethod = "GET"
-
-                println("\nSent 'GET' request to URL : $url; Response Code : $responseCode")
-
-                return listOf(Result.success(inputStream.bufferedReader()))
-            }
-        } catch (e: Exception) {
-            return listOf(Result.failure(e))
+        return try {
+            val reader: BufferedReader = content()
+            listOf(Result.success(reader))
+        } catch (e: UnknownHostException) {
+            listOf(Result.failure(e))
         }
     }
 }
--- src/main/kotlin/no/example/service/Page.kt
+++ src/main/kotlin/no/example/service/Page.kt
@@ -1,15 +1,11 @@
 package no.example.service
 
 import java.io.BufferedReader
-import java.io.InputStream
-import java.io.InputStreamReader
-import java.io.StringWriter
-import java.nio.charset.Charset
 import java.util.stream.Collectors
 
 
 class Page {
-    lateinit var page: String
+    var page: String
 
     constructor(inputStream: BufferedReader) {
         page = inputStream.lines().collect(Collectors.joining("\n"))
